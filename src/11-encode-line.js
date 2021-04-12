b684784e959b66b40a416d22437e7ac99d858bd1
RozumAlena <rozum.elena@gmail.com>
2021-04-13 02:58:31 +0300

    feat: add solving for 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(stst) {
+  let str = stst;
+  let newStr = ''; let num = 0;
+  for (let i = 0; i < str.length;) {
+    const letter = str[i];
+    num = 1;
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[j] === letter) {
+        num += 1;
+      } else {
+        break;
+      }
+    }
+    str = str.replace(letter.repeat(num), '');
+    num = (num !== 1) ? num : '';
+    newStr += (num + letter);
+  }
+  return newStr;
 }
 
 module.exports = encodeLine;
